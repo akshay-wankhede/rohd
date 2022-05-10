4848c0d9986cf127088eda5d04db1e13e13d3fb6
Max Korbel <max.korbel@intel.com>
2022-05-10 16:25:22 -0700

    improved testing for external modules

--- test/external_test.dart
+++ test/external_test.dart
@@ -11,9 +11,11 @@
 import 'dart:io';
 
 import 'package:rohd/rohd.dart';
+import 'package:rohd/src/utilities/simcompare.dart';
 import 'package:test/test.dart';
 
 class MyExternalModule extends ExternalSystemVerilogModule {
+  Logic get b => output('b');
   MyExternalModule(Logic a, {int width = 2})
       : super(
             topModuleName: 'external_module_name',
@@ -21,16 +23,28 @@ class MyExternalModule extends ExternalSystemVerilogModule {
     addInput('a', a, width: width);
     addOutput('b', width: width);
   }
+
+  static String get testExternalVerilog => '''
+module external_module_name #(parameter int WIDTH=2) (
+  input [WIDTH-1:0] a,
+  output [WIDTH-1:0] b
+);
+
+assign b = ~a;
+
+endmodule
+
+''';
 }
 
 class TopModule extends Module {
   TopModule(Logic a) {
     a = addInput('a', a, width: a.width);
-    MyExternalModule(a);
+    var b = addOutput('b', width: a.width);
+    b <= MyExternalModule(a, width: a.width).b;
   }
 }
 
-//TODO: add a test that actually simulates with an external module
 //TODO: add tests that use various types of parameters (external types, ints, etc.)
 
 void main() {
@@ -45,17 +59,51 @@ void main() {
               'external_module_name #(.WIDTH(2)) external_module(.a(a),.b(b));'));
     });
 
-    // TODO: fix checking on this external test
     test('circt', () async {
       var mod = TopModule(Logic(width: 2));
       await mod.build();
       var sv = CirctSynthesizer.convertCirctToSystemVerilog(
           mod.generateSynth(CirctSynthesizer()));
-      File('tmp.sv').writeAsStringSync(sv);
-      // expect(
-      //     sv,
-      //     contains(
-      //         'external_module_name #(.WIDTH(2)) external_module(.a(a),.b(b));'));
+      expect(sv, contains('external_module_name #('));
+      expect(sv, contains(".WIDTH(64'd2)"));
+    });
+  });
+
+  group('simulate', () {
+    var vectors = [
+      Vector({'a': 0xff}, {'b': 0}),
+      Vector({'a': 0xa5}, {'b': 0x5a}),
+    ];
+
+    var signalToWidthMap = {'a': 8, 'b': 8};
+
+    tearDown(() {
+      Simulator.reset();
+    });
+
+    test('native rohd sv generated', () async {
+      var mod = TopModule(Logic(width: 8));
+      await mod.build();
+      var simResult = SimCompare.iverilogVector(
+          MyExternalModule.testExternalVerilog +
+              mod.generateSynth(SystemVerilogSynthesizer()),
+          mod.runtimeType.toString(),
+          vectors,
+          signalToWidthMap: signalToWidthMap);
+      expect(simResult, equals(true));
+    });
+
+    test('circt sv generated', () async {
+      var mod = TopModule(Logic(width: 8));
+      await mod.build();
+      var simResult = SimCompare.iverilogVector(
+          MyExternalModule.testExternalVerilog +
+              CirctSynthesizer.convertCirctToSystemVerilog(
+                  mod.generateSynth(CirctSynthesizer())),
+          mod.runtimeType.toString(),
+          vectors,
+          signalToWidthMap: signalToWidthMap);
+      expect(simResult, equals(true));
     });
   });
 }
